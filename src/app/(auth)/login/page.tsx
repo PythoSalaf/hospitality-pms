9aaa36351e4a00aeb4da0753a02917a87e781510
kaasikodes <thisbroisfresh@gmail.com>
2024-01-01 13:13:43 +0100

    chore: add initial app folder structure

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div className="container flex flex-col gap-y-5 mx-auto text-3xl ">
+      <span>Login</span>
+    </div>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Page() {
+  return (
+    <main className="container flex flex-col gap-y-5 mx-auto text-3xl ">
+      <span>Lendsqr</span>
+      <span className="font-avenir">Lendsqr</span>
+      <span className="font-avenir font-bold ">Lendsqr</span>
+      <span>Lendsqr</span>
+    </main>
+  );
+}
