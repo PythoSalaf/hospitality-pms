f9be0a2c069ebbd82d5109c7659ba0bffa550999
kaasikodes <thisbroisfresh@gmail.com>
2024-01-01 05:56:16 +0100

    chore: adds learning guide file for personal use

---
+++ LEARNING_GUIDE.ts
@@ -0,0 +1,10 @@
+// This repo key learning points are as follows:
+// 1. authentication in next js
+// 2. Styling using tailwind n shad cdn
+// 3. middleware n routes
+
+// Things I ougnt to put time into learning:
+// 1. eslint n prettier setup 4 a project
+// 2. integration of husky, ci/cd, testing(e2e especially cypress) -> refer to vid that showcases the ultimate react setup
+// 3. React hook forms -> especially complex form setups and testing them as well
+// 4. React tables
