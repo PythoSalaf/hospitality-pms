2e9acee0b67c08eff052af3f16f34872a40509af
kaasikodes <thisbroisfresh@gmail.com>
2024-01-03 03:28:13 +0100

    chore(auth): creates verification page

---
+++ src/app/(auth)/verify/page.tsx
@@ -0,0 +1,9 @@
+import AuthContainer from "../_components/AuthContainer";
+
+export default function Page() {
+  return (
+    <AuthContainer title="Verification!" description="Veriying link .....">
+      Verification Processing ....
+    </AuthContainer>
+  );
+}
