f364987ec44239f9742af739b43d6a7b294520f2
kaasikodes <thisbroisfresh@gmail.com>
2024-01-03 06:11:24 +0100

    chore: updates env config

--- src/config/enviroment.ts
+++ src/config/enviroment.ts
@@ -1,7 +1,16 @@
+const DEFAULT_NODE_ENV = "development";
+
 const ENV = {
+  // env
+  NODE_ENV: process.env.NODE_ENV ?? DEFAULT_NODE_ENV,
+  // next
   NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET ?? "",
   NEXTAUTH_URL: process.env.NEXTAUTH_URL ?? "",
-  NODE_ENV: process.env.NODE_ENV ?? "",
+  NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL ?? "",
+
+  // mail
+  RESEND_API_KEY: process.env.RESEND_API_KEY ?? "",
+  MAIL_FROM_EMAIL: process.env.MAIL_FROM_EMAIL ?? "",
 };
 
 export default ENV;
