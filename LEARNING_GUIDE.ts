b020c6cd17ec8779ee3d9071c08fe70e4370e049
kaasikodes <thisbroisfresh@gmail.com>
2024-01-03 03:30:53 +0100

    chore: updates learning guide

--- LEARNING_GUIDE.ts
+++ LEARNING_GUIDE.ts
@@ -8,3 +8,26 @@
 // 2. integration of husky, ci/cd, testing(e2e especially cypress) -> refer to vid that showcases the ultimate react setup
 // 3. React hook forms -> especially complex form setups and testing them as well
 // 4. React tables
+// 5. tailwind config : theme, container(center, padding, screens), plugins, darkmode ....
+// 6. useTransition, also all other useful hooks from web dev simplified
+// 7. 00p js and classes in typescript
+
+// Proposed Workflow:
+// 1. Favicon n meta data
+// 2. Fonts & Colors, Containers, themes(if needed) : tailwind config
+// 3. Components & Providers
+// 4. Database
+// 5. Authentication
+// *. Ensure responsiveness all the way
+
+// Proposed Learning Workflow:
+// - Next 1 project (Great design and all)
+// - Node 1 project , oop js and all
+// - Golang 1 project, all the way
+
+// - Correct Font & Design Adjustments
+// - Implement authentication
+// - Write Unit tests & Integration tests(if need be)
+// - Implement E2E tests with cypress
+// - Implement CI/CD with github actions
+// - Implement husky(what for) and linting
