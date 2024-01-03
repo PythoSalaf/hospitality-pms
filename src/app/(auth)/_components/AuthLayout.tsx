44bfd80ff12ce0fbad969b82fb5c44aea40de3b3
kaasikodes <thisbroisfresh@gmail.com>
2024-01-03 03:23:44 +0100

    chore: ensures responsive components

--- src/app/(auth)/_components/AuthLayout.tsx
+++ src/app/(auth)/_components/AuthLayout.tsx
@@ -6,8 +6,8 @@ import AppLogo from "../../../components/AppLogo";
 const AuthLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
     <div className="h-screen w-full flex ">
-      <div className="flex-1 flex flex-col justify-center px-4 py-2">
-        <div className="ml-8">
+      <div className="flex-1 hidden lg:flex flex-col justify-start gap-y-16 px-4 py-2">
+        <div className="ml-8 lg:mt-16">
           <AppLogo text={{ value: "lendsqr" }} />
         </div>
         <Image
@@ -15,10 +15,20 @@ const AuthLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
           alt="Mascot"
           height={300}
           width={500}
-          className="object-contain mix-blend-multiply self-center mt-16"
+          className="object-contain w-full mix-blend-multiply  self-center"
         />
       </div>
-      <div className="flex-1 bg-white">{children}</div>
+      <div className="flex-1 bg-white flex justify-center items-center px-4 ">
+        <div className="lg:w-[38vw] w-[80vw]">
+          <div className="lg:hidden mb-6 flex justify-end">
+            <AppLogo
+              text={{ value: "lendsqr" }}
+              image={{ height: 18, width: 18 }}
+            />
+          </div>
+          {children}
+        </div>
+      </div>
     </div>
   );
 };
