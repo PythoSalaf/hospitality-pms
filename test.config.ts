0b46a3e51af5d115769d5aa618fb3aba49272c94
kaasikodes <thisbroisfresh@gmail.com>
2024-01-08 03:58:15 +0100

    fix(test): fixes issue with css not being applied

--- test.config.ts
+++ test.config.ts
@@ -1,3 +1,4 @@
+import "./src/app/globals.css";
 import "@testing-library/jest-dom/vitest";
 
 // this is to ensure the extension of vitest assertion to include react testing library assertions from jest dom i guess
--- vitest.config.ts
+++ vitest.config.ts
@@ -14,6 +14,7 @@ export default defineConfig({
   },
   test: {
     globals: true,
+    css: true,
     environment: "jsdom",
     setupFiles: ["./test.config.ts"],
   },
