9a82c75b48ed1a1f88d95b47b6160aebe7d2e04e
kaasikodes <thisbroisfresh@gmail.com>
2024-01-08 04:00:38 +0100

    chore: updates constants

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -4,3 +4,17 @@ export const DEFAULT_LOGO_URL = LendQrLogo;
 export const DEFAULT_LOGO_ALT = "logo";
 export const DEFAULT_LOGO_HEIGHT = 25;
 export const DEFAULT_LOGO_WIDTH = 25;
+
+// tailwind
+export const DEFAULT_TAILWIND_CONFIG = {
+  screens: {
+    sm: 576,
+    // => @media (min-width: 576px) { ... }
+
+    md: 960,
+    // => @media (min-width: 960px) { ... }
+
+    lg: 1440,
+    // => @media (min-width: 1440px) { ... }
+  },
+};
