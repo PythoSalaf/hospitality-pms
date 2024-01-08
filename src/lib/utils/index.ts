1a9213b5bb12761054e62e0cc3594450d4c37e5f
kaasikodes <thisbroisfresh@gmail.com>
2024-01-08 03:55:25 +0100

    chore: adds a createScreenQueryMatchMedia util fn

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -1,6 +1,22 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
+import mediaQuery from "css-mediaquery";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export const createScreenQueryMatchMedia = (width: number) => {
+  return (query: string) => {
+    return {
+      matches: mediaQuery.match(query, { width }),
+      media: "",
+      addListener: () => {},
+      removeListener: () => {},
+      onchange: () => {},
+      addEventListener: () => {},
+      removeEventListener: () => {},
+      dispatchEvent: () => true,
+    };
+  };
+};
