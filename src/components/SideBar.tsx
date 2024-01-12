3c90d2a4709a8fd8995e2b253a16fbe3c1fea10e
kaasikodes <thisbroisfresh@gmail.com>
2024-01-12 15:40:45 +0100

    chore: adds background highlight to sidebarItem

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -11,13 +11,13 @@ import { appRoutes } from "~~/routes";
 import HomeIcon from "./icons/HomeIcon";
 import { usePathname } from "next/navigation";
 
-const SIDEBAR_WIDTH_CLASS_NAME = "w-[20vw]";
+export const SIDEBAR_WIDTH_CLASS_NAME = "min:w-2/12 w-[20vw]";
 const BEFORE_4_SIDEBAR_ITEM =
-  " before:absolute before:py-2 before:right-0 before:left-0 before:-z-10 before:w-[20vw] before:bg-highlight-lightest before:border-highlight before:border-l-8  before:content-['.'] before:text-transparent";
+  " before:absolute before:py-4 before:-mt-3 before:right-0 before:left-0 before:-z-10 before:w-[20vw] before:bg-highlight-lightest before:border-highlight before:border-l-8  before:content-['.'] before:text-transparent";
 const SideBar = () => {
   return (
     <div
-      className={`relative overflow-y-auto scrollBar bg-white flex flex-col min:w-2/12 ${SIDEBAR_WIDTH_CLASS_NAME} h-[100vh] shadow-md ${GENERAL_LAYOUT_CONTAINER_PADDING}`}
+      className={`fixed top-0 overflow-y-auto scrollBar bg-white flex flex-col  ${SIDEBAR_WIDTH_CLASS_NAME} h-[100vh] shadow-md ${GENERAL_LAYOUT_CONTAINER_PADDING}`}
     >
       {/* spacer to account 4 topbar */}
       <div className="md:mt-[15vh]" />
@@ -36,10 +36,10 @@ const SideBar = () => {
       <div className={``}>
         {SIDEBAR_LINK_CATEGORY_ITEMS.map((category, i) => (
           <div className=" mb-6" key={i}>
-            <h2 className="relative text-sm font-worksans text-[#545F7D] font-medium tracking-tight uppercase mb-3">
+            <h2 className="relative text-sm font-worksans text-[#545F7D] font-medium tracking-tight uppercase mb-4">
               {category.title}
             </h2>
-            <div className="space-y-1 flex flex-col gap-1">
+            <div className="space-y-1 flex flex-col gap-y-3">
               {category.items?.map((item, i) => (
                 <SideBarItem item={item} key={i} />
               ))}
@@ -56,20 +56,24 @@ const SideBarItem: React.FC<{ item: TSideBarLink; onClick?: () => void }> = ({
 }) => {
   const pathname = usePathname();
   const url = item?.url ?? "";
+  const activeCondition =
+    pathname.toLowerCase().indexOf(url.toLowerCase()) !== -1 &&
+    url.length !== 0;
+
   return (
     // TODO: Add trasisition animation for the before element
     <Link
       href={url}
       className={` ${
-        new RegExp(pathname, "i").test(url)
+        activeCondition
           ? `opacity-95 before:block ${BEFORE_4_SIDEBAR_ITEM}`
-          : `opacity-60 hover:opacity-40 active:opacity-40 focus:opacity-40 hover:before:block before:hidden ${BEFORE_4_SIDEBAR_ITEM}`
+          : `opacity-70 hover:opacity-60 active:opacity-60 focus:opacity-60 hover:before:block before:hidden ${BEFORE_4_SIDEBAR_ITEM}`
       }`}
     >
       <Button
         variant="ghost"
         className={`w-full font-worksans justify-start font-normal px-3 flex gap-x-2 items-center hover:text-primary active:text-primary hover:bg-transparent ${
-          new RegExp(pathname, "i").test(url) ? "text-primary" : ""
+          activeCondition ? "text-primary" : ""
         }`}
       >
         {item.icon}
