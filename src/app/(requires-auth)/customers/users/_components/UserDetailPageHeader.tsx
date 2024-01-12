bee77a38c279096e8d1a28246c0b3145fee63a79
kaasikodes <thisbroisfresh@gmail.com>
2024-01-12 21:40:08 +0100

    refactor: UserDetail header uses PageHeader

--- src/app/(requires-auth)/customers/users/_components/UserDetailPageHeader.tsx
+++ src/app/(requires-auth)/customers/users/_components/UserDetailPageHeader.tsx
@@ -1,30 +1,16 @@
 import React from "react";
+import PageHeader from "~~/components/page/PageHeader";
 import PageTitle from "~~/components/page/PageTitle";
 import { Button } from "~~/components/ui/button";
+import BlacklistUser from "./userActionButtons/BlacklistUser";
+import ActivateUser from "./userActionButtons/ActivateUser";
 
 const UserDetailPageHeader = () => {
   return (
-    <div
-      className={`flex flex-col gap-6 md:flex-row justify-between w-full my-6 lg:my-8`}
-    >
-      <PageTitle text="User Details" />
-      <div className={`flex  gap-4`}>
-        <Button
-          variant={"outline"}
-          className={`border-destructive text-destructive  uppercase text-xs font-semibold bg-transparent hover:bg-transparent hover:border-black tracking-wider`}
-          // size={`sm`}
-        >
-          Blacklist User
-        </Button>
-        <Button
-          variant={"outline"}
-          className={`border-highlight text-highlight uppercase text-xs font-semibold bg-transparent hover:bg-transparent hover:border-black tracking-wider`}
-          // size={`sm`}
-        >
-          Activate User
-        </Button>
-      </div>
-    </div>
+    <PageHeader
+      title={{ text: "User Details" }}
+      actions={[<BlacklistUser />, <ActivateUser />]}
+    />
   );
 };
 
