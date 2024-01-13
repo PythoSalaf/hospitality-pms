4e87c690e83ee9abf701fa7a7b16d7ecfaaabbb2
kaasikodes <thisbroisfresh@gmail.com>
2024-01-13 08:37:35 +0100

    chore: updates utils with helper fns

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -21,3 +21,33 @@ export const createScreenQueryMatchMedia = (width: number) => {
     };
   };
 };
+
+export function shortenString(props: {
+  input: string;
+  maxLength?: number;
+}): string {
+  const { input, maxLength = 12 } = props;
+  if (input.length <= maxLength) {
+    return input; // No need to shorten
+  }
+
+  const ellipsis = "...";
+
+  // Ensure there's enough room for the ellipsis
+  if (maxLength < ellipsis.length) {
+    return input.substr(0, maxLength); // Just truncate without ellipsis
+  }
+
+  // Calculate the length of the truncated string
+  const truncatedLength = maxLength - ellipsis.length;
+
+  // Get the truncated portion of the string and add ellipsis
+  const truncatedString = input.substring(0, truncatedLength);
+
+  // Ensure the ellipsis doesn't get added if it's already at the end
+  if (truncatedString.endsWith(" ")) {
+    return truncatedString.trim() + ellipsis; // Remove extra space before ellipsis
+  }
+
+  return truncatedString + ellipsis;
+}
