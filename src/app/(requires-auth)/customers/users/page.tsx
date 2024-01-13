28586bbe464cb823d7332677f7bf1dd424cd2c20
kaasikodes <thisbroisfresh@gmail.com>
2024-01-13 08:38:57 +0100

    chore: adds user table to users page

--- src/app/(requires-auth)/customers/users/page.tsx
+++ src/app/(requires-auth)/customers/users/page.tsx
@@ -1,11 +1,17 @@
 import PageHeader from "~~/components/page/PageHeader";
 import { UserCards } from "./_components/UserCard";
+import UsersTable from "./_components/UsersTable";
+import { Card, CardContent } from "~~/components/ui/card";
 
 const Page = () => {
   return (
     <div className="">
       <PageHeader title={{ text: "Users" }} />
-      <UserCards />
+      <div className="flex flex-col gap-y-6 lg:gap-y-8">
+        <UserCards />
+
+        <UsersTable />
+      </div>
     </div>
   );
 };
