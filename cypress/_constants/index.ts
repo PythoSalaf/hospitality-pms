fe26a0968d1ab741dfa69de2efd640c0424899e2
kaasikodes <thisbroisfresh@gmail.com>
2024-01-18 10:12:57 +0100

    chore: defines constants to be used in e2e test

---
+++ cypress/_constants/index.ts
@@ -0,0 +1,12 @@
+export const WRONG_CREDENTIALS = {
+  email: "testuser@example.com",
+  password: "Password123$",
+};
+export const CORRECT_CREDENTIALS = {
+  email: "thisbroisfresh@gmail.com",
+  password: "Hcmatrix123$",
+};
+
+export const PENDING_USER_ID = "6510fb769495301acef683ed"; //Please ensure that is the id of someone whose status is not blacklisted
+export const USERS_API_URL =
+  "https://run.mocky.io/v3/32064e83-fd95-4ac7-ace7-19568daaa806";
