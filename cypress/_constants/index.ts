c4113828c89b77e8ed99ee03e36abda72f890aeb
kaasikodes <thisbroisfresh@gmail.com>
2024-01-18 13:29:40 +0100

    chore: changed users mock api url

--- cypress/_constants/index.ts
+++ cypress/_constants/index.ts
@@ -9,4 +9,4 @@ export const CORRECT_CREDENTIALS = {
 
 export const PENDING_USER_ID = "6510fb769495301acef683ed"; //Please ensure that is the id of someone whose status is not blacklisted
 export const USERS_API_URL =
-  "https://run.mocky.io/v3/32064e83-fd95-4ac7-ace7-19568daaa806";
+  "https://run.mocky.io/v3/ef0b6bab-6a24-4314-9cae-f60e0e40c460";
