48cc41b1b7e862ed937dac68006b9daa48ac22d3
kaasikodes <thisbroisfresh@gmail.com>
2024-01-28 00:37:01 +0100

    chore: updates the test index route with correct info

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,10 @@ import Image from "next/image";
 export default function Page() {
   return (
     <main className="container flex flex-col gap-y-5 mx-auto text-3xl ">
-      <span>Lendsqr</span>
-      <span className="font-avenir">Lendsqr</span>
-      <span className="font-avenir font-bold ">Lendsqr</span>
-      <span>Lendsqr</span>
+      <span>hospitax</span>
+      <span className="font-avenir">hospitax</span>
+      <span className="font-avenir font-bold ">hospitax</span>
+      <span>hospitax</span>
     </main>
   );
 }
